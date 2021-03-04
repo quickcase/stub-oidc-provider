7a0dd5e6caad128daa32f0b162d76b7e6fd71251
Valentin Laurin <valentin.laurin@quickcase.app>
2021-03-04 20:38:04 +0000

    Plug-in OIDC provider with views in Express app

--- app.js
+++ app.js
@@ -1,39 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const loadConfig = require('./src/config');
+const Oidc = require('./src/oidc');
+const UserDB = require('./src/user-db');
 
-var app = express();
+const interactions = require('./src/routes/interactions');
+
+const app = express();
+
+const config = loadConfig();
+const users = UserDB.fromPath(config.USERS_FILE);
+const oidc = Oidc.fromPath(config.CONFIG_FILE)(config.ISSUER, users.findAccount);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use('/interaction', interactions(users, oidc));
+app.use(oidc.callback);
 
 module.exports = app;
---
+++ src/routes/interactions.js
@@ -0,0 +1,104 @@
+const debug = require('debug')('stub-oidc-provider:interactions');
+const express = require('express');
+
+const noCache = (req, res, next) => {
+  res.set('Pragma', 'no-cache');
+  res.set('Cache-Control', 'no-cache, no-store');
+  return next();
+};
+
+const interactions = (users, oidc) => {
+  const router = express.Router();
+
+  router.get('/:uid', noCache, async (req, res, next) => {
+    try {
+      const {uid, params, prompt} = await oidc.interactionDetails(req, res);
+
+      const client = await oidc.Client.find(params.client_id);
+
+      if (prompt.name === 'login') {
+        return res.render('login', {
+          client,
+          uid,
+          prompt,
+          params,
+          flash: undefined,
+        });
+      }
+      return res.render('interaction', {
+        client,
+        uid,
+        prompt,
+        params,
+        scopes: prompt.details.scopes,
+        claims: prompt.details.claims,
+      });
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+  router.post('/:uid/login', express.json(), noCache, async (req, res, next) => {
+    try {
+      const { uid, params, prompt } = await oidc.interactionDetails(req, res);
+
+      const client = await oidc.Client.find(params.client_id);
+
+      const accountId = await users.authenticate(req.body.email, req.body.password);
+
+      if (!accountId) {
+        return res.render('login', {
+          client,
+          uid,
+          prompt,
+          params,
+          flash: 'Invalid email or password.',
+          email: req.body.email,
+        });
+      }
+
+      const result = {
+        login: {
+          account: accountId,
+        },
+      };
+
+      await oidc.interactionFinished(req, res, result, { mergeWithLastSubmission: false });
+
+      debug(`[${uid}] Succesful log-in for ${accountId}`);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.post('/:uid/confirm', noCache, async (req, res, next) => {
+    try {
+      const result = {
+        consent: {},
+      };
+      await oidc.interactionFinished(req, res, result, { mergeWithLastSubmission: true });
+
+      debug(`[${uid}] Consent confirmed`);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.get('/:uid/abort', noCache, async (req, res, next) => {
+    try {
+      const result = {
+        error: 'access_denied',
+        error_description: 'User aborted interaction',
+      };
+      await oidc.interactionFinished(req, res, result, { mergeWithLastSubmission: false });
+
+      debug(`[${uid}] Interaction aborted`);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  return router;
+}
+
+module.exports = interactions;
